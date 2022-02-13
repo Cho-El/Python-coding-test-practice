0ba648adb7b1c4c03df23268f04c615b758430de
Cho_El <syzzang21c@gmail.com>
2022-02-14 03:14:36 +0900

    2022-02-14

--- 기타 코딩 문제/DFS와 BFS/음료수 얼려 먹기.py
+++ 기타 코딩 문제/DFS와 BFS/음료수 얼려 먹기.py
@@ -2,8 +2,9 @@
 N X M 크기의 얼음 틀이 있습니다. 구멍이 뚫려 있는 부분은 0, 칸막이가 존재하는
 부분은 1로 표시됩니다. 구멍이 뚫려 있는 부분끼리 상, 하, 좌, 우로 붙어 있는 경우 서로 연결
 되어 있는 것으로 간주합니다.
-이때 얼음 틀의 모양이 주어졌을 때 생성되는 총 아이스크림의 개수를 구하는 프로그램을 작성하세요
+이때 얼음 틀의 모양이 주어졌을 때 생성되는 총 아이스크림의 개수를 구하는 프로그램을 작성하세요.
 다음의 4 X 5 얼음 틀 예시에서는 아이스크림이 총 3개 생성됩니다.
+
 ex)
 세로 길이 N과 가로 길이 M
 두 번째 줄부터는 N +1번째 줄까지 얼음 틀의 형태가 주어집니다.
@@ -26,7 +27,27 @@ for i in range(N):
 	graph.append(list(map(int, input())))
 
 print(graph)
-for i in graph:
-	print(i)
+# for i in graph:
+# 	print(i)
+def dfs(x, y,n,m):
+	if x <= -1 or x >= n or y <= -1 or y >= m :
+		return False
 
-# def ice_cream():
+	if graph[x][y] == 0:
+		graph[x][y] = 1
+		dfs(x-1,y,n,m)
+		dfs(x,y-1,n,m)
+		dfs(x+1,y,n,m)
+		dfs(x,y+1,n,m)
+		return True
+	return False
+
+def ice_cream(N,M):
+	result = 0
+	for i in range(N):
+		for j in range(M):
+			if dfs(i,j,N,M) == True:
+				result += 1
+	return result
+
+print(ice_cream(N,M))
