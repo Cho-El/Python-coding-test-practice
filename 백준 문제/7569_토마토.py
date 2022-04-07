2e50dabf998b8e430341d39d947a824670e80bb2
Cho-El <68215871+Cho-El@users.noreply.github.com>
2022-04-07 17:44:23 +0900

    2022-04-07

--- 백준 문제/7569_토마토.py
+++ 백준 문제/7569_토마토.py
@@ -1,4 +1,4 @@
-#
+# 그래프 이론, 그래프 탐색, bfs
 import sys
 from collections import deque
 
@@ -13,8 +13,8 @@ def bfs(start):
 
             # 0인 경우 전파, 전 토마토에 + 1해서 대입
             if 0<= nx < n and 0<= ny <m and 0 <= nz < h and graph[nz][nx][ny] == 0:
-                graph[nz][nx][ny] += graph[z][x][y] + 1
-                q.append((nz,nx,ny))
+                graph[nz][nx][ny] = graph[z][x][y] + 1
+                q.append([nz,nx,ny])
 
 
 m,n,h = map(int,input().split())
@@ -34,16 +34,23 @@ for i in range(h):# z
     for j in range(n): # x
         for l in range(m): # y
             if graph[i][j][l] == 1:
-                start.append((i,j,l))
+                start.append([i,j,l])
 
 bfs(start)
-result = 0
 
+result = 0
 for i in graph:
     for j in i:
         if 0 in j:
-            result = 0
-            break
+            print(-1)
+            exit(0) # 
         result = max(result, max(j))
 
-print(result-1)
+# for i in graph:
+#     for j in i:
+#         print(j)
+
+if result == -1:
+    print(-1)
+else:
+    print(result-1)
