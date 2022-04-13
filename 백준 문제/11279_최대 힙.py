fb07f3e12dbe30526646c8e8c10821cfa5e8220b
Cho-El <68215871+Cho-El@users.noreply.github.com>
2022-04-13 15:26:38 +0900

    2022-04-13

---
+++ 백준 문제/11279_최대 힙.py
@@ -0,0 +1,17 @@
+# 힙
+import sys, heapq
+def solution(q,x):
+	if x == 0:
+		if q:
+			print(-heapq.heappop(q))
+			return
+		else:
+			print(0)
+			return
+	heapq.heappush(q,-x)
+if __name__ == '__main__':
+	n = int(input())
+	q = []
+	for _ in range(n):
+		x = int(sys.stdin.readline())
+		solution(q,x)
