953ec53584d0a9a189c66f978d7f94958036850b
Cho-El <syzzang21c@gmail.com>
2022-08-12 15:57:17 +0900

    2022-08-12

--- 백준 문제/16954_움직이는 미로 탈출.py
+++ 백준 문제/16954_움직이는 미로 탈출.py
@@ -1,9 +1,14 @@
 import sys
-
+from collections import deque
 input = sys.stdin.readline
 graph = []
 for i in range(8):
     graph.append(list(input().rstrip()))
 
-for i in graph:
-    print(i)
+answer = 0
+
+def bfs():
+    direction = [[0,0],[0,-1],[0,1],[-1,0],[1,0],[-1,-1],[1,-1],[1,1],[-1,1]]
+    visited = [[0] * 8 for _ in range(8)]
+    dq = deque([7,0,0])
+    
