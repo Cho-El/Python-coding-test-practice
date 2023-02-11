687ff61aa23af2a81c576ed8a1dda2b9a415531f
Cho-El <syzzang21c@gmail.com>
2023-02-11 09:28:59 +0900

    Update 4-1.py

--- 프로그래머스 문제/파이썬/2023 카카오블라이드채용/4-1.py
+++ 프로그래머스 문제/파이썬/2023 카카오블라이드채용/4-1.py
@@ -11,25 +11,27 @@ def makeTree(num, startIdx, endIdx):
         return None
     
     rootIdx = (startIdx + endIdx) // 2
-    root = num[rootIdx]
+    root = int(num[rootIdx])
     leftTree = makeTree(num,startIdx, rootIdx - 1)
     rightTree = makeTree(num, rootIdx + 1, endIdx)
     
     return Tree(root, leftTree, rightTree)
 
-def checkTree(tree):
+def checkTree(tree, parent):
     global check
+    
     # leaf 노드인지 체크
-    if tree.left == None or tree.right ==None:
+    if tree == None:
         return
     
-    # 아니라면 root가 0인지 체크
-    if tree.value == '0':
-        check = False
-        return
+    # 부모 노드가 0인데 자식의 값은 1인 경우
+    if parent == 0:
+        if tree.value == 1:
+            check = False
+            return
     
-    checkTree(tree.left)
-    checkTree(tree.right)
+    checkTree(tree.left, tree.value)
+    checkTree(tree.right, tree.value)
     
 def solution(numbers):
     global check
@@ -37,19 +39,25 @@ def solution(numbers):
     for n in numbers:
         targetNum = bin(n)[2:]
         i = 0
+        
         while 2 ** i < len(targetNum):
             i += 1
         targetNum = '0' * (2 ** i - 1 - len(targetNum)) + targetNum
         tree = makeTree(targetNum,0,len(targetNum) - 1)
-        check = True
-        checkTree(tree)
-        if check:
-            result.append(1)
-        else:
+        tree = makeTree('1100000',0,6)
+        
+        if tree.value == 0: # 루트 노드가 0인경우
             result.append(0)
+        else: # 아닌 경우 
+            check = True
+            checkTree(tree, 1)
+            if check:
+                result.append(1)
+            else:
+                result.append(0)
     
     return result
-        
+
     
 if __name__ == "__main__":
     numbers = [[7,42,5],[63,111,95]]
