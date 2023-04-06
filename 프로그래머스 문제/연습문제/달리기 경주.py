4ad4b3337f9a18667d59f74d9d83559a849a4e3f
Cho-El <68215871+Cho-El@users.noreply.github.com>
2023-04-07 03:39:29 +0900

    Update 달리기 경주.py

--- 프로그래머스 문제/연습문제/달리기 경주.py
+++ 프로그래머스 문제/연습문제/달리기 경주.py
@@ -1,5 +1,11 @@
 def solution(players, callings):
     answer = []
+    playersDict = {}
+    for idx, p in enumerate(players):
+        playersDict[idx] = p
+    
+    for c in callings:
+        playersDict[c]
     return answer
 
 print(solution(["mumu", "soe", "poe", "kai", "mine"],["kai", "kai", "mine", "mine"]))
