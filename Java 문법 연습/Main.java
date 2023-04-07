92b502b9ebbdefa5e8ca51fcb0fb2b4b5faaae07
Cho-El <syzzang21c@gmail.com>
2023-04-07 15:36:40 +0900

    자바 문법 연습 세팅

---
+++ Java 문법 연습/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        String[] str = solution.solution();
+        System.out.println((str[0] + str[1]).getClass().getName());
+    }
+}
---
+++ Java 문법 연습/Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public String[] solution() {
+        String[] answer = {"1","2"};
+        return answer;
+    }
+}
