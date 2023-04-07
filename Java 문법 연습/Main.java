fbe44b8a5fd98b15533170315ebd82840b1be2b7
Cho-El <syzzang21c@gmail.com>
2023-04-07 23:39:51 +0900

    자바 문법 연습

--- Java 문법 연습/Main.java
+++ Java 문법 연습/Main.java
@@ -1,8 +1,16 @@
+import java.net.SocketPermission;
+
 public class Main {
     public static void main(String[] args){
-        ExternalClass solution = new ExternalClass();
-        String[] strArray = solution.returnPuStringArray();
-        System.out.println(solution.puNum);
-        System.out.println(strArray[0]);
-    } 
+        final ExternalClass externalClass = new ExternalClass(); 
+        //  externalClass = new ExternalClass(); // final로 선언했으므로 재할당 불가능
+        String[] strArray = externalClass.returnPuStringArray();
+        System.out.println("public int : " + externalClass.puNum);
+        System.out.println("private int : " + externalClass.getprivateNum());
+        System.out.print("배열 :");
+        for(String s: strArray) {
+            System.out.print(" " + s);
+        }
+        System.out.println();
+    }
 } 
