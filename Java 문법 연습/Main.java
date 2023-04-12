ee1b41900f552c0e28152c0d343174f7c5bdc9e1
Cho-El <68215871+Cho-El@users.noreply.github.com>
2023-04-12 20:58:50 +0900

    자바 문법 연습
    DI 연습

--- Java 문법 연습/Main.java
+++ Java 문법 연습/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        final ExternalClass externalClass = new ExternalClass(100,200); 
+        final ExternalClass externalClass = new ExternalClass(100,200);
         //  externalClass = new ExternalClass(); // final로 선언했으므로 재할당 불가능
+        final Main2 main2 = new Main2(externalClass);
         String[] strArray = externalClass.returnPuStringArray();
         System.out.println("public int : " + externalClass.puNum);
         System.out.println("private int : " + externalClass.getprivateNum());
@@ -12,11 +13,25 @@ public class Main {
         System.out.println();
         System.out.println("privateNum : " + externalClass.getprivateNum());
         executePrivateMethodInStaticMethod();
+        Main2.executeNonStaticPublicMethodInStaticMethod();
+        main2.printGetPrivateNuminMain2();
+
     }
     private static void executePrivateMethodInStaticMethod() {
         System.out.println("private Method execute in static method");
     }
-    public static void executeNonStaticPublicMethodInStaticMethod() {
-        System.out.println("Non Static Public Method execute in static method");
+    
+}
+class Main2 {
+    private ExternalClass externalClass;
+
+    public Main2(ExternalClass externalClass) {
+        this.externalClass = externalClass;
     }
-} 
+    public void printGetPrivateNuminMain2() {
+        System.out.println("externalClassPrivateInt print in Main2 : " + this.externalClass.getprivateNum());
+    }
+    public static void executeNonStaticPublicMethodInStaticMethod() {
+        System.out.println("Static Public Method execute in static method");
+    }
+}
