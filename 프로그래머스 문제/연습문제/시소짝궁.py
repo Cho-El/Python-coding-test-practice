db245beff4f52ecf11f3ff683ac38057cdda7ce6
Cho-El <68215871+Cho-El@users.noreply.github.com>
2023-04-12 16:19:27 +0900

    프로그래머스 문제 풀이
    dictionary 초기화 기법:
    .get(넣을 값,0)을 통해 초기화한다.

--- 프로그래머스 문제/연습문제/시소짝궁.py
+++ 프로그래머스 문제/연습문제/시소짝궁.py
@@ -1,3 +1,14 @@
 def solution(weights):
+    weight_dict = dict()
     answer = 0
+    for w in weights: weight_dict[w] = weight_dict.get(w, 0) + 1
+    for w in weights:
+        if w % 2 == 0: answer += weight_dict.get(w * 3 // 2, 0)
+        if w % 3 == 0: answer += weight_dict.get(w * 4 // 3, 0)
+        answer += weight_dict.get(w * 2, 0)
+    for w in weights:
+        weight_dict[w] -= 1
+        answer += weight_dict[w]
     return answer
+
+print([100,180,360,100,270])
