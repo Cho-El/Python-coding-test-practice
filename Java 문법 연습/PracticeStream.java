420f005229b65b67823d1c9c4c3f993f50577aea
Cho-El <syzzang21c@gmail.com>
2023-04-16 11:03:12 +0900

    자바 stream 연습

--- Java 문법 연습/PracticeStream.java
+++ Java 문법 연습/PracticeStream.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.sound.sampled.SourceDataLine;
@@ -20,6 +22,7 @@ public class PracticeStream {
         System.out.println("strArray2 : " + Arrays.toString(strArray2));
         System.out.println("strArray3 : " + strArray3);
         System.out.println("strArray3 : " + Arrays.toString(strArray3));
+        System.out.println("strArray3 length : " + strArray3.length);
 
         // int 초기화
         int[] intArray1 = new int[5]; // 초기값 0
@@ -52,6 +55,11 @@ public class PracticeStream {
         List<Integer> arrayList1 = new ArrayList<>();
         List<Integer> arrayList2 = new ArrayList<>(Arrays.asList(1,2,3,4,5));
         List<Integer> arrayList3 = new ArrayList<>(setInteger);
+        List<Integer> arrayList4 = new ArrayList<>() {{
+            add(5);
+            add(6);
+            add(7);
+        }};
 
         // 데이터 삽입
         arrayList1.add(2);
@@ -67,5 +75,53 @@ public class PracticeStream {
         System.out.println("arrayList1 : " + arrayList1);
         System.out.println("arrayList2 : " + arrayList2);
         System.out.println("arrayList3 : " + arrayList3);
+        System.out.println("arrayList4 : " + arrayList4);
+        System.out.println("arraList1 size : " + arrayList1.size());
+        
+        // 데이터 순회
+        for (Integer arrayListE : arrayList4) { // for each 문
+            System.out.print(arrayListE + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < arrayList4.size(); i++) {
+            System.out.print(arrayList4.get(i) + " ");
+        }
+        System.out.println();
+        arrayList4.forEach((element) -> {
+            System.out.println("element : " + element);
+        });
+        arrayList4.stream().forEach((element) -> System.out.println("element : " + element));
+        arrayList4.stream().forEach(System.out::println);
+
+        // HashMap--------------------------------------------
+        // 초기화 생성
+        Map<String, Integer> hashMap1 = new HashMap<>();
+        Map<String, Object> hashMap2 = new HashMap<>() {{
+            put("name", "yoon");
+            put("nickname", "cho-el");
+        }}; // 이 방식을 많이 활용
+        Map<String, Object> immutableMap = Map.of(
+            "score", "32",
+            "nickname", "june"
+        ); // 개수가 10개까지 제한이 있다.
+        Map<Integer, String> immutableMap2 = Map.ofEntries(
+            Map.entry(1,"kim"),
+            Map.entry(2, "cho"),
+            Map.entry(3,"na")
+        ); // 10 개 이상 초기화 가능
+        System.out.println();
+        System.out.println(immutableMap);
+
+        // 데이터 삽입 및 수정
+        hashMap1.put("a", 1);
+        hashMap1.put("b", 2);
+        hashMap1.put("c",3);
+        
+        hashMap2.put("email", "syzzang21c@naver.com");
+        hashMap2.put("name", "Na");
+        // immutableMap.put("score", "40"); Map.of로 생성한 인스턴스는 수정할 수 없다.
+        
+        // 데이터 순회
+        
     }
 }
--- 프로그래머스 문제/파이썬/2023 네이버 공채/2.py
+++ 프로그래머스 문제/파이썬/2023 네이버 공채/2.py
@@ -1,16 +1,13 @@
 from collections import deque
 def solution(n,k):
-    answer = 0\
+    answer = 0
     
     return answer
 
 def dfs(array,k):
-    array.sort(reverse = True)
     tempArray = []
-    while array:
-        num = array.pop()
-        if num > k:
-            addArray = [num - k, k]
-            dfs(array + addArray ,k)
+    for _ in range(len(array)):
+        num = array.popleft()
+            
     
 print(solution(7,3))
