b0a046e3857a75354203d8cd9d766ff25925dcf5
Cho-El <68215871+Cho-El@users.noreply.github.com>
2023-04-26 17:21:30 +0900

    Update 1000.js

--- 자바스크립트 연습/1000.js
+++ 자바스크립트 연습/1000.js
@@ -1,45 +1,62 @@
-const fs = require('fs');
-const inputData = fs.readFileSync(__dirname + "/example.txt").toString().split(' ');
+// const fs = require('fs');
+// const inputData = fs.readFileSync(__dirname + "/example.txt").toString().split(' ');
 
-const input = ['472','385','111'].map(parseInt)
+// const input = ['472','385','111'].map(parseInt)
 
-let a = [1,2,3,4].map((a,b,c,d) => a + b)
+// let a = [1,2,3,4].map((a,b,c,d) => a + b)
 
-let test = [1,2]
-test.push(10)
-console.log(test) // [1,2,10]
-let popElement = test.pop()
-console.log(popElement) // 10
-console.log(test) // [1,2]
+// let test = [1,2]
+// test.push(10)
+// console.log(test) // [1,2,10]
+// let popElement = test.pop()
+// console.log(popElement) // 10
+// console.log(test) // [1,2]
 
-test = [2,3,4,5]
-console.log(test.join('\n'))
+// test = [2,3,4,5]
+// console.log(test.join('\n'))
 
-let test2 = 'a b c d'
-let test3 = test2.replace('a','h')
+// let test2 = 'a b c d'
+// let test3 = test2.replace('a','h')
 
-console.log(test3)
-let newArray = test2.split()
-console.log(newArray)
+// console.log(test3)
+// let newArray = test2.split()
+// console.log(newArray)
 
-console.log('\\    /\\')
-console.log(' )  ( \')')
-console.log('(  /  )')
-console.log(' \\(__)|')
+// console.log('\\    /\\')
+// console.log(' )  ( \')')
+// console.log('(  /  )')
+// console.log(' \\(__)|')
 
 
-var array1 = ['1','2','3']
-var changeArray = array1.map((x) => Number(x))
-console.log(changeArray)
+// var array1 = ['1','2','3']
+// var changeArray = array1.map((x) => Number(x))
+// console.log(changeArray)
 
-console.log(false)
+// console.log(false)
 
-let str1 = "a"
-let int1 = 1
-console.log(`str1 : ${str1}, int1 : ${int1}`)
+// let str1 = "a"
+// let int1 = 1
+// console.log(`str1 : ${str1}, int1 : ${int1}`)
 
-let array = new Array()
-let N = 10
-for (let i = 0; i < N; i++) {
-    array[i] = i + 1
+// let array = new Array()
+// let N = 10
+// for (let i = 0; i < N; i++) {
+//     array[i] = i + 1
+// }
+
+
+function solution() {
+    let result = 0;
+    for (let i = 1; i <= 10000; i++) {
+        let target = i
+        while (target > 0) {
+            if (target % 10 === 8) {
+                result++;
+            }
+            target = parseInt(target / 10);
+        }
+    }
+    return result
 }
+let value = solution()
+console.log(value)
